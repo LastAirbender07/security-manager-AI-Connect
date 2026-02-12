0f446a8a4fb603cab8c925a8b156c8aa82355137
I750332 <jayaraj.viswanathan@sap.com>
2026-02-12 16:21:16 +0530

    update cors

--- security-manager-main/backend/main.py
+++ security-manager-main/backend/main.py
@@ -16,7 +16,6 @@ from fastapi.middleware.cors import CORSMiddleware
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
